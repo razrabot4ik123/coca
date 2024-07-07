e4e4fdaa229dffe754fe75fed05ca303d7c53732
razrabot4ik123 <IIIRaZRaBoT4IKIII@yandex.ru>
2024-07-08 02:23:36 +0700

    Created strength and partners sections and added slider in partners section

---
+++ src/js/components/slider.js
@@ -0,0 +1,40 @@
+import Swiper from 'swiper';
+import 'swiper/css';
+import { Autoplay } from 'swiper/modules';
+
+export const usePartnersSlider = () => {
+  function checkWidth() {
+    if (window.innerWidth <= 768) {
+      initSwiper();
+    }
+  }
+  checkWidth();
+
+  function initSwiper() {
+    new Swiper('.partners__slider', {
+      modules: [Autoplay],
+      speed: 1000,
+      loop: true,
+      slidesPerView: 1.5,
+      spaceBetween: 15,
+      centeredSlides: true,
+      autoplay: {
+        delay: 3000,
+        pauseOnMouseEnter: true,
+        disableOnInteraction: false,
+      },
+
+      breakpoints: {
+        650: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+
+        450: {
+          slidesPerView: 2,
+          spaceBetween: 30,
+        },
+      },
+    });
+  }
+};
--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
+import { usePartnersSlider } from './components/slider.js';
+usePartnersSlider();
 import '../assets/scss/main.scss';
 
 // components
-import { useTheme } from './components/theme';
-import { useBurger } from './components/burger';
-import { useHeader } from './components/header';
+import { useTheme } from './components/theme.js';
+import { useBurger } from './components/burger.js';
+import { useHeader } from './components/header.js';
 
 useTheme();
 useBurger();
