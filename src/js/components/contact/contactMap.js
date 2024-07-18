59546f875747976066c03df36aa4a7d299d2bd74
razrabot4ik123 <IIIRaZRaBoT4IKIII@yandex.ru>
2024-07-18 16:42:12 +0700

    Added styles to yandex map

--- src/js/components/contact/contactMap.js
+++ src/js/components/contact/contactMap.js
@@ -1,17 +1,107 @@
 export const useMap = () => {
+  const themeTarget = document.querySelector('[data-theme]');
+  const theme = localStorage.getItem('theme') || 'light';
+
+  const themeTargetDataset = themeTarget.dataset;
+  themeTargetDataset.theme = theme;
+
   async function initMap() {
     await ymaps3.ready;
 
-    const { YMap, YMapDefaultSchemeLayer } = ymaps3;
+    const { YMap, YMapDefaultSchemeLayer, YMapDefaultFeaturesLayer } = ymaps3;
 
     const map = new YMap(document.getElementById('map'), {
       location: {
-        center: [60, 28],
+        center: [110, 1],
         zoom: 2,
       },
     });
 
-    map.addChild(new YMapDefaultSchemeLayer());
+    const featuresLayer = new YMapDefaultFeaturesLayer();
+    map.addChild(featuresLayer);
+
+    function layerDark() {
+      const layerDark = new YMapDefaultSchemeLayer({
+        customization: [
+          {
+            tags: {
+              all: ['water'],
+            },
+            stylers: [
+              {
+                color: '#1d1e25',
+              },
+            ],
+          },
+          {
+            elements: 'label',
+            stylers: [
+              {
+                visibility: 'off',
+              },
+            ],
+          },
+          {
+            tags: {
+              all: ['landscape'],
+            },
+            stylers: [
+              {
+                color: '#acacb9',
+              },
+            ],
+          },
+        ],
+      });
+      map.addChild(layerDark);
+      window.layerDark = layerDark;
+    }
+
+    function layerLight() {
+      const layerLight = new YMapDefaultSchemeLayer({
+        customization: [
+          {
+            tags: {
+              all: ['water'],
+            },
+            stylers: [
+              {
+                color: '#FFF',
+              },
+            ],
+          },
+          {
+            elements: 'label',
+            stylers: [
+              {
+                visibility: 'off',
+              },
+            ],
+          },
+          {
+            tags: {
+              all: ['landscape'],
+            },
+            stylers: [
+              {
+                color: '#acacb9',
+              },
+            ],
+          },
+        ],
+      });
+      map.addChild(layerLight);
+      window.layerLight = layerLight;
+    }
+
+    if (theme === 'dark') {
+      layerDark();
+    } else {
+      layerLight();
+    }
+
+    window.YMapDefaultSchemeLayer = YMapDefaultSchemeLayer;
+    window.map = map;
   }
   initMap();
 };
